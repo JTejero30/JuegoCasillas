9169af5087d9042e43d9dfbd721313ccecbe5f43
Javi Tejero <javitejero@93gmail.com>
2023-03-19 10:30:00 +0100

    2.0

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-let main, form, boton, label, dificultad, casillas, nCasillas, tablero
+let main, form, boton, label, dificultad, casillas, nCasillas, tablero, icono, body, contador
 
 let input = document.createElement('input');
 
 let arrayCasillas = [];
+let posicionJugador1, posicionJugador2, objetivo;
+let marcador1 = 0, marcador2 = 0;
+
 main = document.getElementsByTagName('main')[0];
 tablero = document.getElementsByClassName('tablero')[0];
+body = document.getElementsByTagName('body')[0];
+
+icono = document.createElement('i')
+icono.setAttribute('class', "far fa-flag")
+
+boton = document.createElement('button')// el boton lleva a la funcion reset que repinta 
+boton.addEventListener('click', reset)// boton insertado en el objetivo
+
+contador = document.getElementsByClassName('contador')[0]
 
 
+
+
+// tablero.appendChild(i);
 
 // function inicio() {
 
@@ -32,61 +47,259 @@ tablero = document.getElementsByClassName('tablero')[0];
 //     boton.addEventListener('click', crearCasillas);
 // }
 
+/**********************************************************/
+/*********************CREAR TABLERO************************ */
+/**********************************************************/
+crearCasillas();
+cuentaAtras();
+
+function cuentaAtras() {
+    x = 4;
+    contador.textContent = 'Preparados'
+    var objeto = setInterval(function () {
+
+        if (x == 0) {
+            document.addEventListener('keydown', mover)
+            contador.textContent = ""
+        } else {
+            x--;
+            contador.textContent = x;
+
+        }
+
+    }, 1000);
+}
+
+
+body.addEventListener('load', cuentaAtras);
 
 function crearCasillas() {
 
-    // dificultad = input.value
-    console.log(nCasillas)
-    for (let i = 0; i < 10; i++) {
-        arrayCasillas.push([])
-        for (let j = 0; j < 15; j++) {
-            arrayCasillas[i].push(casillas)
-            casillas = document.createElement('div');
-            casillas.setAttribute('class', 'casilla');
-            tablero.appendChild(casillas);
-        }
+    for (let i = 0; i < 150; i++) {
+        casillas = document.createElement('div');
+        casillas.setAttribute('class', 'casilla');
+        tablero.appendChild(casillas)
+        arrayCasillas.push(casillas)
     }
-    console.log(arrayCasillas);
 
 }
-crearCasillas()
 
-function pintarJugadores() {
-    let posicionJugador1, posicionJugador2, objetivo
 
-    posicionJugador1 = [Math.floor(Math.random() * 10), Math.floor(Math.random() * 15)]
-    posicionJugador2 = [Math.floor(Math.random() * 10), Math.floor(Math.random() * 15)]
-    objetivo = [Math.floor(Math.random() * 10), Math.floor(Math.random() * 15)]
+/*******************************************************************/
+/*********************POSICIONES ALEATORIAS************************ */
+/****************************************************************/
+function posicionJugadores() {
 
-    arrayCasillas[posicionJugador1[0]][posicionJugador1[1]].style.backgroundColor = 'green'
-    arrayCasillas[posicionJugador2[0]][posicionJugador2[1]].style.backgroundColor = 'orange'
-    arrayCasillas[objetivo[0]][objetivo[1]].style.backgroundColor = 'blue'
+    posicionJugador1 = Math.floor(Math.random() * 150)
+    posicionJugador2 = Math.floor(Math.random() * 150)
+    objetivo = Math.floor(Math.random() * 150)
 
+
+    if (posicionJugador1 != posicionJugador2 || posicionJugador1 != objetivo || posicionJugador2 != objetivo) {
+        color();
+    }
+
+}
+//si los jugadores se chocan se pinta de nuevo el tablero y no puntua nadie 
+
+function choque() {
     if (posicionJugador1 == posicionJugador2) {
-        pintarJugadores()
+        reiniciarContador()
+        quitarColor();
+        posicionJugadores();
     }
 }
-pintarJugadores();
 
-document.addEventListener('keydown', mover)
+/**********************************************************/
+/*********************COLOR************************ */
+/**********************************************************/
 
+/*Funciones que sin parametros nos sirven para colorear y quitar color una vez sacadas las posiciones
+en la funcion anterior. Estan separadas de las posiciones porque en ocasiones nos interesa una
+y en ocasiones otra*/
+function color() {
+    if (posicionJugador1 != posicionJugador2 && posicionJugador1 != objetivo && posicionJugador2 != objetivo) {
+        arrayCasillas[posicionJugador1].style.backgroundColor = '#3DBFF2'
+        arrayCasillas[posicionJugador1].textContent = marcador1;//marcador incluido
+        arrayCasillas[posicionJugador2].style.backgroundColor = 'orange'
+        arrayCasillas[posicionJugador2].textContent = marcador2;//marcador incluido
+        arrayCasillas[objetivo].style.backgroundColor = '#51D92B'
+
+        arrayCasillas[objetivo].appendChild(boton);// un boton que se ubicara donde el objetivo y que reiniciara la partida
+        // boton.textContent='Reset';
+        boton.appendChild(icono);
+
+        boton.setAttribute('class', 'reset');
+    }else{
+        choque()// si coinciden las posiciones llama a choque que es como repintar las casillas
+    }
+}
+function quitarColor() {
+    arrayCasillas[posicionJugador1].style.backgroundColor = 'aliceblue'
+    arrayCasillas[posicionJugador1].textContent = "";//para ir quitandole el resultado a la que se mueve
+    arrayCasillas[posicionJugador2].style.backgroundColor = 'aliceblue'
+    arrayCasillas[posicionJugador2].textContent = "";
+    arrayCasillas[objetivo].style.backgroundColor = 'aliceblue'
+    arrayCasillas[objetivo].textContent = "";
+}
+
+posicionJugadores();
+
+/**********************************************************/
+/*********************MOVER FICHA************************ */
+/**********************************************************/
+
+
+
+/**Funcion que recibe una tecla y actua en base a ella
+ * Para las posiciones de los bordes gira por el tablero, ejemplo si la casilla
+ * es la 149 y presiona right se le resta 9 y vuelve a la 140.
+ * Si la casilla es la 5 y presiona up se le suma 140 y pasa a la 145.
+ */
 function mover(parametro) {
+    choque();
     switch (parametro['key']) {
+
         case 'ArrowUp':
-            console.log('tecla arriba');
+            if (posicionJugador1 < 10) {
+                quitarColor();
+                posicionJugador1 = posicionJugador1 + 140
+                color();
+                ganar();
+
+            } else {
+                quitarColor();
+                posicionJugador1 = posicionJugador1 - 10
+                color();
+                ganar();
+            }
             break;
         case 'ArrowDown':
-            console.log('tecla abajo');
+            if (posicionJugador1 >= 140) {
+                quitarColor();
+                posicionJugador1 = posicionJugador1 - 140
+                color();
+                ganar();
+
+            } else {
+                quitarColor();
+                posicionJugador1 = posicionJugador1 + 10
+                color();
+                ganar();
+            }
             break;
         case 'ArrowLeft':
-            console.log('tecla izquierda');
+            if (posicionJugador1 % 10 == 0) {
+                quitarColor()
+                posicionJugador1 = posicionJugador1 + 9
+                color();
+                ganar();
+            } else {
+                quitarColor();
+                posicionJugador1 = posicionJugador1 - 1
+                color();
+                ganar();
+            }
             break;
         case 'ArrowRight':
-            console.log('tecla derecha');
+            if (posicionJugador1 % 10 == 9) {
+                quitarColor()
+                posicionJugador1 = posicionJugador1 - 9
+                color();
+                ganar();
+            } else {
+                quitarColor();
+                posicionJugador1 = posicionJugador1 + 1
+                color();
+                ganar();
+            }
+            break;
+        case 'w':
+            if (posicionJugador2 < 10) {
+                quitarColor();
+                posicionJugador2 = posicionJugador2 + 140
+
+                color();
+                ganar();
+
+            } else {
+                quitarColor();
+                posicionJugador2 = posicionJugador2 - 10
+                color();
+                ganar()
+            }
+            break;
+        case 's':
+            if (posicionJugador2 >= 140) {
+                quitarColor();
+                posicionJugador2 = posicionJugador2 - 140
+                color();
+                ganar();
+
+            } else {
+                quitarColor();
+                posicionJugador2 = posicionJugador2 + 10
+                color();
+                ganar()
+            }
+            break;
+        case 'a':
+            if (posicionJugador2 % 10 == 0) {
+                quitarColor()
+                posicionJugador2 = posicionJugador2 + 9
+                color();
+                ganar();
+            } else {
+                quitarColor();
+                posicionJugador2 = posicionJugador2 - 1
+                color();
+                ganar();
+            }
+            break;
+        case 'd':
+            if (posicionJugador2 % 10 == 9) {
+                quitarColor()
+                posicionJugador2 = posicionJugador2 - 9
+                color();
+                ganar();
+            } else {
+                quitarColor();
+                posicionJugador2 = posicionJugador2 + 1
+                color();
+                ganar()
+            }
             break;
 
-        default: 
-        
+        default:
+
             break;
     }
+
+}
+
+/*Funcion que comprueba si alguna de las dos fichas comparte posicion con el objetivo*/
+function ganar() {
+    if (posicionJugador1 == objetivo) {
+        marcador1++
+        quitarColor()
+        posicionJugadores()
+        reiniciarContador()// reiniciamos contador
+
+    } else if (posicionJugador2 == objetivo) {
+        marcador2++
+        quitarColor()
+        posicionJugadores()
+        reiniciarContador()
+        // reiniciamos contador
+    }
+}
+
+function reiniciarContador() {
+    x = 4
+    document.removeEventListener('keydown', mover)
+}
+
+function reset() {
+    quitarColor()
+    posicionJugadores()
 }
